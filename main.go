561da626b1dd28ec46d5b37233afaf0f3ee4bfdc
Joe Legeckis <thejml@gmail.com>
2022-06-28 23:52:46 -0400

    Logging now works.

--- main.go
+++ main.go
@@ -1,30 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"time"
 )
 
-func main() {
+type LogFormat struct {
+	Timestamp  time.Time `json:"timestamp"`
+	Method     string    `json:"method"`
+	RequestURI string    `json:"request"`
+	//	Path       string        `json:"internalPath"`
+	Duration time.Duration `json:"duration"`
+}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+// func Logger(inner http.Handler, name string) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		start := time.Now()
+
+// 		//inner.ServeHTTP(w, r)
+
+// 		hit := LogFormat{start, r.Method, r.RequestURI, name, time.Since(start)}
+// 		entry, _ := json.Marshal(hit)
+
+// 		log.Print(string(entry))
+
+// 	})
+// }
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("x-api-key") == "deadbeef" {
+		fmt.Fprintf(w, "Success")
+	} else {
+		http.Error(w, "Authorization Failed", http.StatusUnauthorized)
+	}
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		hit := LogFormat{start, r.Method, r.RequestURI, time.Duration(time.Since(start))}
+		entry, _ := json.Marshal(hit)
+
+		log.Print(string(entry))
+		handler.ServeHTTP(w, r)
 	})
+}
+
+func main() {
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi")
 	})
 
-	http.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("x-api-key") == "deadbeef" {
-			fmt.Fprintf(w, "Success")
-		} else {
-			http.Error(w, "Authorization Failed", 401)
-		}
-	})
+	http.HandleFunc("/auth", authHandler)
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8081", logRequest(http.DefaultServeMux)))
 
 }
